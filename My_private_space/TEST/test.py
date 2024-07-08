1a66e0f45dd3e1046799a3e1617e8020ce59cbc1
Eng-YosefT <eng.yosef.t@gmail.com>
2024-07-08 03:11:17 +0300

    add my files

--- My_private_space/TEST/test.py
+++ My_private_space/TEST/test.py
@@ -1,5 +1,11 @@
 name="youseef"
+age = 20
 print (len(name))
 print(type(name))
-name = name.capitalize
+name = name.capitalize()
 print(name)
+print("your name is {} and age is {} one".format(name,age))
+print(F"name is {name} and age {age}two")
+print("your name is {1} and age is {0} th".format(name,age))
+print("your name is %s and age is %d forth"%(name,age))
+print("name is :",name,"age is : ",age)
